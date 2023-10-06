2298b7b6994d36943295c5d6973984b12517c64e
DanGonzaG <dan.g.g@hotmail.es>
2023-10-05 23:26:30 -0600

    EA-08-Practicas-2 Daniel González Guerreo

---
+++ src/main/java/com/Edilit/EdilitApplication.java
@@ -0,0 +1,13 @@
+package com.Edilit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdilitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdilitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Edilit/EdilitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Edilit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdilitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
